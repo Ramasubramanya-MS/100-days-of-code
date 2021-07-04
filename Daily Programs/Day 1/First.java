f6ca7ddd5f9a66e45328264bfa571af2b337cf08
Ramasubramanya-MS <81099796+Ramasubramanya-MS@users.noreply.github.com>
2021-07-04 12:09:28 +0530

    Delete Daily Programs/Day 1 directory

--- Daily Programs/Day 1/First.java
+++
@@ -1,6 +0,0 @@
-public class First{
-  public static void main(String[] args)
-  {
-    System.out.println("Hare Krishna, All Glories to Srila Prabhupada");
-  }
-}
--- Daily Programs/Day 1/Main.java
+++
@@ -1,5 +0,0 @@
-public class Main{
-  public static void main(String[] args){
-    System.out.println("Hare Krishna Prabhu, All glories to Srila Prabhupada");
-  }
-}
